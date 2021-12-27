0dc5ae3d367e2edbb6f392a75bea5919913cd320
Sagar Bhatia <sagarbhatia722@gmail.com>
2021-12-27 15:20:52 +0530

    procfile

--- server/services/render.js
+++ server/services/render.js
@@ -5,7 +5,7 @@ exports.homeRoutes = (req, res) => {
   axios
     .get("http://localhost:3000/api/users")
     .then(function (response) {
-      console.log(response.data);
+      console.log(response.data); 
       res.render("index", { users: response.data });
     })
     .catch((err) => {
