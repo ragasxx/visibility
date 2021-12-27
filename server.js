920ce8fbfcc2dafc861c3c770df16453d47ac51e
Sagar Bhatia <sagarbhatia722@gmail.com>
2021-12-27 17:04:44 +0530

    changes in port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const path = require("path");
@@ -8,8 +8,8 @@ const connectDB = require("./server/database/connection");
 
 const app = express();
 
-dotenv.config({ path: "config.env" });
-let port = process.env.PORT;  
+ dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 3000;  
 
 // log requests
 // app.use(morgan("tiny"));
@@ -32,10 +32,8 @@ app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 // load routers
 app.use("/", require("./server/routes/router"));
 
-if (port == null || port == "") {
-  port = 8080;
-}
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
