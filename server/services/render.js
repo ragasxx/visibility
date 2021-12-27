667079270cfabc430e9370e684684541a394f654
Sagar Bhatia <sagarbhatia722@gmail.com>
2021-12-27 16:25:11 +0530

    changes in render

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 exports.homeRoutes = (req, res) => {
   // Make a get request to /api/users
   axios
-    .get(`${window.location.hostname}:3000/api/users`)
+    .get('/api/users')
     .then(function (response) {
       console.log(response.data); 
       res.render("index", { users: response.data });
@@ -19,7 +19,7 @@ exports.add_user = (req, res) => {
 
 exports.update_user = (req, res) => {
   axios
-    .get(`${window.location.hostname}:3000/api/users`, { params: { id: req.query.id } })
+    .get('/api/users', { params: { id: req.query.id } })
     .then(function (userdata) {
       res.render("update_user", { user: userdata.data });
     })
